1ab321e58f49f28ac80067d501f329e396aff197
nuclearnic <nicholas@nuclearnic.net>
2021-02-21 12:42:03 +0200

    update

--- lib/marking/module.rb
+++ lib/marking/module.rb
@@ -5,9 +5,15 @@ require_relative "module/version"
 module Marking
   module Module
     class Error < StandardError; end
-    # Your code goes here...
-    def self.generate
-      puts 'hello world'
+
+    class Generator < Rails::Generators::NamedBase
+      include Rails::Generators::ResourceHelper
+      desc "Generates MarkingModule"
     end
+    # Your code goes here...
+    # def self.generate
+    #   puts 'hello world'
+    # end
   end
 end
+
